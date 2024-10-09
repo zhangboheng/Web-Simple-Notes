83c44702392c5f2c22163560ded84004674d72cd
luckydesigner <zhangboheng827@gmail.com>
2024-10-09 22:28:25 +0800

    Update SQL File

--- sql/notestable.sql
+++ sql/notestable.sql
@@ -29,7 +29,7 @@ CREATE TABLE `manageruser` (
   `loginpwd` varchar(255) DEFAULT NULL,
   `remarks` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,37 @@ CREATE TABLE `manageruser` (
 
 LOCK TABLES `manageruser` WRITE;
 /*!40000 ALTER TABLE `manageruser` DISABLE KEYS */;
+INSERT INTO `manageruser` VALUES (5,'John','zhangboheng','$2a$10$NM24Vg2wqsGdWrOD.CGTTuCmkVIadh6gS2qpAPEFmIhRsKNHO4O2i',NULL);
 /*!40000 ALTER TABLE `manageruser` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `note_history`
+--
+
+DROP TABLE IF EXISTS `note_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `note_history` (
+  `history_id` bigint NOT NULL AUTO_INCREMENT,
+  `note_id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` tinytext NOT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`history_id`),
+  KEY `note_id` (`note_id`),
+  CONSTRAINT `note_history_ibfk_1` FOREIGN KEY (`note_id`) REFERENCES `notes` (`note_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `note_history`
+--
+
+LOCK TABLES `note_history` WRITE;
+/*!40000 ALTER TABLE `note_history` DISABLE KEYS */;
+INSERT INTO `note_history` VALUES (37,10,'测试发达的身上','12213123','2024-10-09 05:12:33'),(38,10,'测试发达的身上','12213123','2024-10-09 05:12:33'),(39,10,'测试发达的身上','12213123','2024-10-09 05:12:33'),(40,10,'测试发达的身上','12213123','2024-10-09 05:12:33'),(41,10,'测试发达的身上','12213123','2024-10-09 05:12:33'),(44,10,'测试发达的身上','12213123','2024-10-09 05:12:34'),(45,10,'测试发达的身上','12213123','2024-10-09 05:12:49'),(46,10,'测试发达的adsf','12213123','2024-10-09 05:41:22'),(47,10,'测试发达的adsf','12213123sdfdsaf','2024-10-09 05:57:50'),(48,10,'New Note For You','12213123sdfdsaf','2024-10-09 06:20:25');
+/*!40000 ALTER TABLE `note_history` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -51,14 +81,14 @@ DROP TABLE IF EXISTS `notes`;
 CREATE TABLE `notes` (
   `note_id` int NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `content` text COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`note_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `manageruser` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,6 +97,7 @@ CREATE TABLE `notes` (
 
 LOCK TABLES `notes` WRITE;
 /*!40000 ALTER TABLE `notes` DISABLE KEYS */;
+INSERT INTO `notes` VALUES (10,5,'New Note For You','12213123sdfdsaf','2024-10-06 19:20:17','2024-10-09 06:20:25'),(12,5,'New Note','','2024-10-08 08:01:47','2024-10-08 08:01:47'),(13,5,'New Note','57h','2024-10-08 08:09:54','2024-10-09 03:34:30'),(14,5,'New Note','','2024-10-09 05:45:46','2024-10-09 05:45:46'),(15,5,'New Note','','2024-10-09 05:45:46','2024-10-09 05:45:46'),(16,5,'New Note','','2024-10-09 05:45:47','2024-10-09 05:45:47'),(17,5,'New Note','','2024-10-09 05:45:47','2024-10-09 05:45:47'),(18,5,'New Note','','2024-10-09 05:45:47','2024-10-09 05:45:47'),(19,5,'New Note','','2024-10-09 05:45:48','2024-10-09 05:45:48');
 /*!40000 ALTER TABLE `notes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,4 +118,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-05 17:59:49
+-- Dump completed on 2024-10-09 22:27:56
